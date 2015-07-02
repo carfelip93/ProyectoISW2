ffadf230deeaa64690ff221f860bf2a71fcfaddc
carfelip93 - Carlos Velozo <c.velozoweinberger@gmail.com>
2015-07-02 00:04:15 -0400

    Actu Nuget

---
+++ ProyectoISW2/ProyectoISW2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ProyectoISW2.MvcApplication" Language="C#" %>
---
+++ ProyectoISW2/ProyectoISW2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProyectoISW2.Startup))]
+namespace ProyectoISW2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
